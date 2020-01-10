8d2f245f60062cd886aeb615361319d8b62ec27f
Swishyfishie <pclandreiuk@gmail.com>
2020-01-10 02:33:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Dog
   # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+  def initialize(name, owner, mood="nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@all.push(self)
+  end 
+
+  def self.all 
+    @@all
+  end 
+
+  def dogs 
+    Dog.all.select {|dog| dog.owner == self}   
+  end 
+
+  def buy_dog(name)
+    new_puppy = Dog.new(name, self)
+   end 
+  # binding.pry
 end
