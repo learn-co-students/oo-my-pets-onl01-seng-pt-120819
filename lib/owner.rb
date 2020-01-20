f71268173d7f4c862dfebdff5296823c0c38b27a
Alexis Priore <alexispriore@gmail.com>
2020-01-20 18:02:56 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,81 @@
 class Owner
-  # code goes here
+  
+  attr_reader :species
+  attr_accessor :name, :pets, :dog, :cat
+
+  @@all = []
+  @@pets = {:dogs => [], :cats => []}
+
+  def initialize(species)
+    @species = species
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def pets
+    @@pets
+  end
+
+  def buy_cat(cat)
+    @@pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    @@pets[:dogs] << Dog.new(dog)
+  end
+
+  def walk_dogs
+    @@pets[:dogs].each do |dog|
+      dog.mood = 'happy'
+    end
+  end
+
+  def play_with_cats
+    @@pets[:cats].each do |cat|
+      cat.mood = 'happy'
+    end
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
+
+  def sell_pets
+    pets.each do |pet, arr|
+      arr.map do |pet|
+        pet.mood = 'nervous'
+      end
+      arr.clear
+    end
+  end
+
+end
+
+human = Owner.new("human")
+human2 = Owner.new("human2")
+
+Owner.all
+
+human.buy_cat("fluffy")
+human.buy_cat("whiskers")
+human.buy_dog("fido")
+human.buy_dog("maestro")
+
+human.list_pets
 end
