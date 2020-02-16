03aa1d69fc5ba40110ed4172f63c19cb5d0a7a4c
William Pannell <lpann5@gmail.com>
2020-02-16 00:40:30 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all=[]
+  def initialize(name, species="human")
+    @name=name
+    @species=species
+    save
+  end
+  
+  def say_species
+    message="I am a #{@species}."
+    message
+  end
+  
+  def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    Cats.all
+  end
 end
